6f2bd22db9e80c6229b13bb8fa59422a114c54ab
Kanak <kanakkumarmahala000@gmail.com>
2026-01-12 21:37:10 +0530

    feat : change in team

--- src/components/TeamSection.tsx
+++ src/components/TeamSection.tsx
@@ -13,7 +13,7 @@ type TeamMember = {
 const teamMembers: TeamMember[] = [
     {
         name: "Saneeta Narrlla",
-        role: "Founder & Technologist, CodeBuzz • Data Security • AI Education",
+        role: "Founder & Technologist, CodeBuzz\nData • Security • AI Education",
         bgColor: "bg-[#ffe8b8]", // Light yellow/orange
         image: "/teams/sneetal-narala.jpeg",
     },
